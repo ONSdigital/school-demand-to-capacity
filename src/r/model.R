d29e14d0a64c0f72067f44383dfff9cd67afca89
Thomas-Sadler <thomas.sadler@ons.gov.uk>
2017-03-27 17:03:54 +0100

    test: LA data = ward data by LA?

--- src/r/model.R
+++ src/r/model.R
@@ -89,9 +89,23 @@ primary_school_demand_by_geography <- calculate_demand_by_geography(geography, G
                       expect_equal(count_wards, 215)
                       }))
 
+(results <- test_that("Test that the number of pupils per LA is the same in the LA data set and the ward data set",
+                      {# set geography to LA, make test dataframe (rename of primary_school_demand_by_geography)
+                      geography = "LA"
+                      GM_Primary_school_population_by_area_and_age <- select_primary_school_population_by_geography(geography)
+                      test_demand_from_LA_data <- calculate_demand_by_geography(geography, GM_Primary_school_population_by_area_and_age)
+                      # set geography to ward, make primary_school_demand_by_geography, sum by LA to make test dataframe, rename columns
+                      geography = "ward"
+                      GM_Primary_school_population_by_area_and_age <- select_primary_school_population_by_geography(geography)
+                      primary_school_demand_by_geography <- calculate_demand_by_geography(geography, GM_Primary_school_population_by_area_and_age)
 
-
-
+                      test_demand_from_ward_data <- primary_school_demand_by_geography %>% 
+                        group_by(Local.Authority) %>%
+                        summarise(demand=sum(demand)) %>%
+                        setnames("Local.Authority", "LA")
+                      # compare test dataframes, should be the same
+                      expect_equal(test_demand_from_LA_data, test_demand_from_ward_data)
+                      }))
 
 (results <- test_that("Test that the number of wards is 215",
                       {number_wards <-nrow(calculate_capacity_by_geography(geography = "ward"))
