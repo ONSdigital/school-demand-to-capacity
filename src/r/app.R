337725c61168540b6ec99a5b5dbada00e1b91d0e
Seb Bailey <sebastian@mastodonc.com>
2017-04-05 17:27:34 +0100

    Comment out tests

--- src/r/app.R
+++ src/r/app.R
@@ -18,26 +18,26 @@ library(shiny)
 ### functions
 select_boundaries_by_geography <- function(geography){
   if (geography == "ward") {
-    boundaries <- readShapePoly("data/clean/Ward_shapefile_GM/GM_Ward.shp")
+    boundaries <- readShapePoly("data/Ward_shapefile_GM/GM_Ward.shp")
   } else {
     if (geography == "LA") {
-      boundaries <- readShapePoly("data/clean/LA_shapefile_GM/GM_LA.shp")
+      boundaries <- readShapePoly("data/LA_shapefile_GM/GM_LA.shp")
     }
   }
 }
 
 select_primary_school_population_by_geography <- function(geography){
   if (geography == "ward") {
-    population <- read.csv("data/clean/ward_population.csv", stringsAsFactors = F)
+    population <- read.csv("data/ward_population.csv", stringsAsFactors = F)
   } else {
     if (geography == "LA") {
-      population <- read.csv("data/clean/LA_Population.csv", stringsAsFactors = F)
+      population <- read.csv("data/LA_Population.csv", stringsAsFactors = F)
     }
   }
 }
 
 calculate_capacity_by_geography <- function(geography){
-  capacity <- read.csv("data/clean/GM_schools_dataset.csv", stringsAsFactors = F)
+  capacity <- read.csv("data/GM_schools_dataset.csv", stringsAsFactors = F)
   if (geography == "ward") {
     aggregated_capacity <- aggregate(capacity$Netcapacity..1, by=list(Geography=capacity$Ward.Code), FUN=sum)
     aggregated_capacity <- setnames(aggregated_capacity, "x", "aggregated.net.capacity")
@@ -159,48 +159,48 @@ plot_choropleth_map <- function(GM_boundaries_with_school_data){
 ### plot with user parameter specified from the command line
 #plot_choropleth_map(run_model(geography = "ward"))
 
-#Tests
-
-(results <- test_that("Test that the number of wards in demand set is the number in GM (=215)",
-                      {geography = "ward"
-                      GM_Primary_school_population_by_area_and_age <- select_primary_school_population_by_geography(geography)
-                      GM_boundaries_by_geography <- select_boundaries_by_geography(geography)
-                      primary_school_demand_by_geography <- calculate_demand_by_geography(geography,
-                                                                                          GM_Primary_school_population_by_area_and_age,
-                                                                                          GM_boundaries_by_geography)
-                      count_wards <- length (unique(primary_school_demand_by_geography$Geography))
-                      expect_equal(count_wards, 215)
-                      }))
-
-(results <- test_that("Test that the number of pupils per LA is the same in the LA data set and the ward data set",
-                      {# set geography to LA, make test dataframe (rename of primary_school_demand_by_geography)
-                        geography = "LA"
-                        GM_Primary_school_population_by_area_and_age <- select_primary_school_population_by_geography(geography)
-                        GM_boundaries_by_geography <- select_boundaries_by_geography(geography)
-                        test_demand_from_LA_data <- calculate_demand_by_geography(geography,
-                                                                                  GM_Primary_school_population_by_area_and_age,
-                                                                                  GM_boundaries_by_geography)
-                        
-                        test_demand_from_LA_data <- test_demand_from_LA_data %>% 
-                          select(LA, demand) %>% # drops LA geo code
-                          arrange(LA)
-                        # set geography to ward, make primary_school_demand_by_geography, sum by LA to make test dataframe, rename columns
-                        geography = "ward"
-                        GM_Primary_school_population_by_area_and_age <- select_primary_school_population_by_geography(geography)
-                        primary_school_demand_by_geography <- calculate_demand_by_geography(geography,
-                                                                                            GM_Primary_school_population_by_area_and_age,
-                                                                                            GM_boundaries_by_geography)
-                        
-                        test_demand_from_ward_data <- primary_school_demand_by_geography %>%
-                          group_by(Local.Authority) %>%
-                          summarise(demand=sum(demand)) %>%
-                          setnames("Local.Authority", "LA") %>%
-                          as.data.frame() %>% # to force into data frame only
-                          arrange(LA)
-                        # compare test dataframes, should be the same
-                        
-                        expect_equal(test_demand_from_LA_data, test_demand_from_ward_data)
-                      }))
+# #Tests
+# 
+# (results <- test_that("Test that the number of wards in demand set is the number in GM (=215)",
+#                       {geography = "ward"
+#                       GM_Primary_school_population_by_area_and_age <- select_primary_school_population_by_geography(geography)
+#                       GM_boundaries_by_geography <- select_boundaries_by_geography(geography)
+#                       primary_school_demand_by_geography <- calculate_demand_by_geography(geography,
+#                                                                                           GM_Primary_school_population_by_area_and_age,
+#                                                                                           GM_boundaries_by_geography)
+#                       count_wards <- length (unique(primary_school_demand_by_geography$Geography))
+#                       expect_equal(count_wards, 215)
+#                       }))
+# 
+# (results <- test_that("Test that the number of pupils per LA is the same in the LA data set and the ward data set",
+#                       {# set geography to LA, make test dataframe (rename of primary_school_demand_by_geography)
+#                         geography = "LA"
+#                         GM_Primary_school_population_by_area_and_age <- select_primary_school_population_by_geography(geography)
+#                         GM_boundaries_by_geography <- select_boundaries_by_geography(geography)
+#                         test_demand_from_LA_data <- calculate_demand_by_geography(geography,
+#                                                                                   GM_Primary_school_population_by_area_and_age,
+#                                                                                   GM_boundaries_by_geography)
+#                         
+#                         test_demand_from_LA_data <- test_demand_from_LA_data %>% 
+#                           select(LA, demand) %>% # drops LA geo code
+#                           arrange(LA)
+#                         # set geography to ward, make primary_school_demand_by_geography, sum by LA to make test dataframe, rename columns
+#                         geography = "ward"
+#                         GM_Primary_school_population_by_area_and_age <- select_primary_school_population_by_geography(geography)
+#                         primary_school_demand_by_geography <- calculate_demand_by_geography(geography,
+#                                                                                             GM_Primary_school_population_by_area_and_age,
+#                                                                                             GM_boundaries_by_geography)
+#                         
+#                         test_demand_from_ward_data <- primary_school_demand_by_geography %>%
+#                           group_by(Local.Authority) %>%
+#                           summarise(demand=sum(demand)) %>%
+#                           setnames("Local.Authority", "LA") %>%
+#                           as.data.frame() %>% # to force into data frame only
+#                           arrange(LA)
+#                         # compare test dataframes, should be the same
+#                         
+#                         expect_equal(test_demand_from_LA_data, test_demand_from_ward_data)
+#                       }))
 
 ## app
 
